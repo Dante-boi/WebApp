f7f49ab63c5cfc6eb9672bf2e9d6ad5f04c2c600
Dante <dantekarlssonbuler@gmail.com>
2026-02-10 10:20:21 +0100

    [Add] stuff and thingamajigs

---
+++ Pages/bigballs.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class bigballsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {    
+        }
+    }
+}
